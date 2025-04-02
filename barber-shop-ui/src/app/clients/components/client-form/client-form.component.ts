46cb3ab490da632a570d618d67d458a25217d94e
Victor Neves <victorneves15@gmail.com>
2025-04-02 17:42:24 -0300

    feat:front-end/ clients components

---
+++ barber-shop-ui/src/app/clients/components/client-form/client-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { NgxMaskDirective } from 'ngx-mask';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { ClientModelForm } from '../../client.models';
+
+@Component({
+    selector: 'app-client-form',
+    imports: [
+        FormsModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        NgxMaskDirective,
+    ],
+    templateUrl: './client-form.component.html',
+    styleUrl: './client-form.component.scss',
+})
+export class ClientFormComponent {
+    @Input() client: ClientModelForm = {
+        id: 0,
+        name: '',
+        email: '',
+        phone: '',
+    };
+
+    @Output() clientSubmited = new EventEmitter<ClientModelForm>();
+
+    onSubmit(_: NgForm) {
+        this.clientSubmited.emit(this.client);
+    }
+}
